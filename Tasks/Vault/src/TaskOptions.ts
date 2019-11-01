04e5a3174b8eaf7cdeb6b8f98555e7f498d33cc0
Joey Lorich <joseph@lorich.me>
2019-10-31 22:16:28 -0400

    Update task options

--- Tasks/Vault/src/TaskOptions.ts
+++ Tasks/Vault/src/TaskOptions.ts
@@ -9,26 +9,5 @@ export class TaskOptions {
 
     // Basic
     readonly command : string | undefined;
-    readonly provider : string | undefined;
-    readonly backend : string | undefined;
-
-    // Azure
-    readonly providerAzureConnectedServiceName : string | undefined;
-    readonly backendAzureUseProviderConnectedServiceForBackend : boolean | undefined;
-    readonly backendAzureConnectedServiceName : string | undefined;
-    readonly backendAzureStorageAccountName : string | undefined;
-    readonly backendAzureProviderStorageAccountName : string | undefined;
-    readonly backendAzureContainerName : string | undefined;
-    readonly backendAzureStateFileKey : string | undefined;
-
-    // CLI
-    readonly scriptLocation : string | undefined;
-    readonly scriptPath : string | undefined;
-    readonly script : string | undefined;
-    readonly initialize : boolean | undefined;
-
-    // Advanced
-    readonly cwd : string | undefined;
-    readonly args : string | undefined;
-    readonly tempDir : string | undefined;
+    readonly vaultToken : string | undefined;
 }
