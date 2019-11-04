3741b8fe961d09a648aca5ac966abd10429ba9aa
Joey Lorich <joseph@lorich.me>
2019-11-04 02:58:42 -0800

    cleaup task typing

--- Tasks/Vault/src/VaultTask.ts
+++ Tasks/Vault/src/VaultTask.ts
@@ -6,6 +6,8 @@ import task = require('azure-pipelines-task-lib/task');
 import { injectable } from "inversify";
 import { VaultClient } from "./VaultClient";
 import { TaskOptions } from './TaskOptions';
+import { KeyValueReadResponse } from "./Engines/KeyValue/KeyValueReadResponse"
+import { KeyValueGetResponse } from "./Engines/KeyValue/KeyValueGetResponse"
 
 @injectable()
 export class VaultTask {
@@ -23,7 +25,7 @@ export class VaultTask {
 
         switch(this.options.command) {
             case 'kvGet':
-                let kvGetResponse = await this.vault.keyValue.get(this.options.key);
+                let kvGetResponse : KeyValueGetResponse = await this.vault.keyValue.get(this.options.key);
                 //todo: add prefix
                 this.setResultVariables(kvGetResponse.data.data, this.options.key);
                 break;
@@ -32,9 +34,9 @@ export class VaultTask {
                 await this.vault.keyValue.put(this.options.key, data)
                 break;
             case 'read':
-                let readResponse = await this.vault.keyValue.read(this.options.key);
+                let readResponse : KeyValueReadResponse = await this.vault.keyValue.read(this.options.key);
                 //todo: add prefix
-                this.setResultVariables(readResponse.data.data, this.options.key);
+                this.setResultVariables(readResponse.data, this.options.key);
                 break;
             case 'write':
                 data = JSON.parse(this.options.data);
